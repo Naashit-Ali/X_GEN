eb9e178fd04e890657d99fe35c448c36750ebd7c
Naashit-Ali <109617827+Naashit-Ali@users.noreply.github.com>
2023-12-28 15:27:08 +0500

    Update Home.jsx

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -90,7 +90,7 @@ function Home() {
 
   const fetchNextItemID = async () => {
     try {
-      const response = await fetch('http://localhost:5000/api/expenses/nextItemID');
+      const response = await fetch('https://x-genback.vercel.app/api/expenses/nextItemID');
       const data = await response.json();
       setNextItemID(data.nextItemID);
     } catch (error) {
@@ -188,7 +188,7 @@ function Home() {
     }
   
     try {
-      const response = await fetch('http://localhost:5000/api/expenses', {
+      const response = await fetch('https://x-genback.vercel.app/api/expenses', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -220,7 +220,7 @@ function Home() {
 
   const fetchExpenses = async () => {
     try {
-      const response = await fetch('http://localhost:5000/api/expenses');
+      const response = await fetch('https://x-genback.vercel.app/api/expenses');
       const data = await response.json();
       setExpenses(data);
     } catch (error) {
@@ -230,7 +230,7 @@ function Home() {
 
   const handleDeleteExpense = async (expenseId) => {
     try {
-      const response = await fetch(`http://localhost:5000/api/expenses/${expenseId}`, {
+      const response = await fetch(`https://x-genback.vercel.app/api/expenses/${expenseId}`, {
         method: 'DELETE',
       });
   
@@ -345,7 +345,7 @@ function Home() {
         return;
       }
 
-      const response = await fetch(`http://localhost:5000/api/expenses/${_id}`, {
+      const response = await fetch(`https://x-genback.vercel.app/api/expenses/${_id}`, {
         method: 'PUT',
         headers: {
           'Content-Type': 'application/json',
