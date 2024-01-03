b42bc0904c31436252d0cde286856ab0d307416a
Naashit-Ali <109617827+Naashit-Ali@users.noreply.github.com>
2024-01-03 18:50:55 +0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,13 +5,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Login.jsx';
 import Home from './Home.jsx';
-import PrivateRoute from './PrivateRoute';
+// import PrivateRoute from './PrivateRoute';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route exact path='/' element={<PrivateRoute/>}>
+{/*           <Route exact path='/' element={<PrivateRoute/>}> */}
             <Route exact path='/home' element={<Home/>}/>
           </Route>
          
