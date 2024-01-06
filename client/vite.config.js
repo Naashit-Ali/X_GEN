6ae25612280f5f6c6a20e5adc2c0f55e0a919c60
Naashit-Ali <109617827+Naashit-Ali@users.noreply.github.com>
2024-01-06 14:26:40 +0500

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/subdirectory/',
   build: {
     chunkSizeWarningLimit: 1000, // Set the limit to 1000 kB (1 MB) or a value that suits your needs
   },
 });
+
